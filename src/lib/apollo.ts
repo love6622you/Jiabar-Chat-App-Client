84e7e1cef368c8a8763cedb0d074c88088ed4322
Chia <lovemaple0913@gmail.com>
2022-05-05 12:58:24 +0800

    [MOD]http -> https

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -25,7 +25,7 @@ const wsLink = new GraphQLWsLink(
 
 const httpLink = new HttpLink({
   // uri: "http://localhost:4000/graphql"
-  uri: "http://jiabar-chat-app.herokuapp.com/graphql"
+  uri: "https://jiabar-chat-app.herokuapp.com/graphql"
 });
 
 const splitLink = split(
