17f04d190ba30e761e49d3cdb0d6232ad223fb42
Chia <lovemaple0913@gmail.com>
2022-05-07 01:41:24 +0800

    [MOD]vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 
+const viteEnv = {};
+Object.keys(process.env).forEach((key) => {
+  if (key.startsWith("VITE_")) {
+    viteEnv[`import.meta.env.${key}`] = process.env[key];
+  }
+});
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -19,6 +26,7 @@ export default defineConfig({
       "@": resolve(__dirname, "src") // 設置 '@' 指向 'src' 目錄
     }
   },
+  define: viteEnv,
   server: {
     port: 8010,
     open: false,
